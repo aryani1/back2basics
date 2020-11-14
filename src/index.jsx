d1d4576f16606e6445e164274af710166bc5ca2e
Aryan Iranzamini <aryaniranzamini@gmail.com>
2020-11-14 15:20:25 +0100

    Add babel config & use React to render

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import _ from "lodash";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function renderApp() {
+  return <div>Hello world!!!</div>;
+}
+
+ReactDOM.render(renderApp, document.getElementById("app"));
